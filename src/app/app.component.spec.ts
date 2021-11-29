38871c4137a32e803f5237dbe71ce13faf6e11a8
Pawel Piotrowski <pawel@flytype.org>
2021-11-29 22:28:20 +0000

    refactor app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { DataService } from './data/data.service';
+import { Faq } from './data/faq';
+
+const dataServiceSpy = jasmine.createSpyObj('DataService', ['fetchFaq']);
+const mockedFaq: Faq = {
+  id: '6',
+  question: 'Test?',
+  answer: 'Yes!',
+};
+
+dataServiceSpy.fetchFaq.and.returnValue(of({ data: [mockedFaq] }));
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      providers: [{ provide: DataService, useValue: dataServiceSpy }],
     }).compileComponents();
   });
 
@@ -26,6 +37,35 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dlg-tech-test app is running!');
+    expect(compiled.querySelector('.app-title')?.textContent).toContain('FAQ');
+  });
+
+  it('should call data service to fetch faqs', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const dataService = TestBed.inject(DataService);
+
+    fixture.detectChanges();
+
+    expect(dataService.fetchFaq).toHaveBeenCalled();
+  });
+
+  describe('onFaqDataLoad', () => {
+    it('should set components faqs if passed data is valid', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      const mockedValidFaqData = { data: [mockedFaq] };
+      // starts empty
+      expect(app.faqs.length).toEqual(0);
+
+      // if called with no data
+      app.onFaqDataLoad({} as any);
+      // remains empty
+      expect(app.faqs.length).toEqual(0);
+      // when called with valid data
+      app.onFaqDataLoad(mockedValidFaqData);
+      // will set as faqs
+      expect(app.faqs.length).toEqual(mockedValidFaqData.data.length);
+      expect(app.faqs[0]).toEqual(mockedValidFaqData.data[0]);
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data/data.service';
+import { Faq, FaqResponse } from './data/faq';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dlg-tech-test';
+  faqs: Faq[] = [];
+
+  constructor(private dataApi: DataService) {}
+
+  ngOnInit(): void {
+    this.dataApi.fetchFaq().subscribe(this.onFaqDataLoad.bind(this));
+  }
+
+  onFaqDataLoad(dataResp: FaqResponse): void {
+    if (dataResp && dataResp.data && dataResp.data.length > 0) {
+      this.faqs = [...dataResp.data];
+    }
+  }
 }
