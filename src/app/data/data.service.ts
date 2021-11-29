b24b9188d0ee458ee7fa321a728b3b765eaa1214
Pawel Piotrowski <pawel@flytype.org>
2021-11-29 23:58:30 +0000

    add synthetic imports

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import * as faqJson from '../../assets/data/faqs.json';
+import faqJson from '../../assets/data/faqs.json';
 import { FaqResponse } from './faq';
 
 @Injectable({
